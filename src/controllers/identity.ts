2992e3c8eb46c21b276c5dac2b64f88f0568e0b6
GauthamSree <51874061+GauthamSree@users.noreply.github.com>
2023-08-08 23:05:37 +0530

    Added all functionality to /identity Endpoint
    - Adding Records if new information is fed
    - Processing the response

--- src/controllers/identity.ts
+++ src/controllers/identity.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
 import pool from '../db.js';
+import { QueryResult } from 'pg';
 
 interface IdentityRequest {
     email?: string;
     phoneNumber?: number;
+}
+
+interface Contact {
+    primaryContactId: number,
+    emails: string[], 
+    phoneNumbers: string[], 
+    secondaryContactIds: number[]
 }
 
 const getWelcomeMessage = (req: Request, res: Response) => {
@@ -14,10 +22,75 @@ const getWelcomeMessage = (req: Request, res: Response) => {
 }
 
 const insertContact = async (email: string, phoneNumber: number, linkedId?: number) => {
-    const result = await pool.query(
+    return await pool.query(
         "INSERT INTO CONTACT (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt) values ($1, $2, $3, $4, to_timestamp($5/1000.0), to_timestamp($6/1000.0))", 
         [phoneNumber, email, linkedId, (linkedId) ? "secondary": "primary", Date.now(), Date.now()]);
-    return result.rows[0];        
+}
+
+const insertNewInfo = async (results: QueryResult, email: string, phoneNumber: number) => {
+    let primaryContactId: number;
+    let unrecordedPhoneNumber: boolean = true;
+    let unrecordedEmail: boolean = true;
+    
+    results.rows.forEach((row) => {
+        if (row['linkprecedence'] === "primary") {
+            primaryContactId = row['id']
+        }
+        if (row['phonenumber'] && row['phonenumber'] == phoneNumber) {
+            unrecordedPhoneNumber = false;
+        }
+        if (row['email'] && row['email'] === email) {
+            unrecordedEmail = false;
+        }
+    })
+
+    if (!unrecordedEmail && !unrecordedPhoneNumber) return;
+    
+    if (unrecordedPhoneNumber) {
+        await insertContact(null, phoneNumber, primaryContactId)
+    } else {
+        await insertContact(email, null, primaryContactId)
+    }
+}
+
+const findRecords = (results: QueryResult) => {
+    let primaryContactId: number;
+
+    if (results.rows[0]['linkprecedence'] === "primary") {
+        primaryContactId = results.rows[0]['id']
+    } else {
+        primaryContactId = results.rows[0]['linkedid']
+    }
+
+    return pool.query(
+        "SELECT * FROM contact WHERE linkedId = $1 OR id = $1", 
+        [primaryContactId]
+    )
+}
+
+
+const processResult = (results: QueryResult) => {
+    let contact: Contact = {
+        emails: [],
+        phoneNumbers: [],
+        secondaryContactIds: [],
+        primaryContactId: -1
+    };
+    
+    results.rows.forEach((row) => {
+        if (row['linkprecedence'] === "primary") {
+            contact.primaryContactId = row['id']
+            if (row['phonenumber']) contact.phoneNumbers.unshift(row['phonenumber'].toString())
+            if (row['email']) contact.emails.unshift(row['email'])
+        } 
+        else {
+            if (row['linkprecedence'] === "secondary") contact.secondaryContactIds.push(row['id'])
+            if (row['phonenumber']) contact.phoneNumbers.push(row['phonenumber'].toString())
+            if (row['email']) contact.emails.push(row['email'])
+        }
+    })
+
+    return contact;
 }
 
 
@@ -27,49 +100,67 @@ const findIdentity = async (req: Request<{}, {}, IdentityRequest>, res: Response
         pool.query(
             "SELECT * FROM contact WHERE email=$1 OR phoneNumber=$2", [email, phoneNumber], 
             async (error, results) => {
-                if (error) {
-                    throw error;   
-                } 
+                if (error) throw error;   
+                
                 if (results.rowCount == 0) {
-                    await insertContact(email, phoneNumber)
+                    results = await insertContact(email, phoneNumber)
+                } else {
+                    await insertNewInfo(results, email, phoneNumber)
+                    results = await findRecords(results)
                 }
+                const result: Contact = processResult(results)
+                res.status(200)
+                .header('Content-Type', 'application/json')
+                .json({
+                    "contact": result
+                })
             }
         )
     } else if (email) {
         pool.query(
             "SELECT * FROM CONTACT WHERE email = $1", [email], 
             async (error, results) => {
-                if (error) {
-                    throw error;   
-                } 
+                if (error) throw error;   
+
                 if (results.rowCount == 0) {
-                    await insertContact(email, phoneNumber)
+                    results = await insertContact(email, phoneNumber)
+                } else {
+                    results = await findRecords(results)
                 }
+                
+                const result: Contact = processResult(results)
+                res.status(200)
+                .header('Content-Type', 'application/json')
+                .json({
+                    "contact": result
+                })
             }
         )
     } else if (phoneNumber) {
         pool.query(
             "SELECT * FROM CONTACT WHERE phoneNumber = $1", [phoneNumber], 
             async (error, results) => {
-                if (error) {
-                    throw error;   
-                } 
+                if (error) throw error;   
+
                 if (results.rowCount == 0) {
-                    await insertContact(email, phoneNumber)
+                    results = await insertContact(email, phoneNumber)
+                } else {
+                    results = await findRecords(results)
                 }
+
+                const result: Contact = processResult(results)
+                res.status(200)
+                .header('Content-Type', 'application/json')
+                .json({
+                    "contact": result
+                })
             }
         )
     } else {
         res.status(400).json({
-            "status": "Failure",
-            "message": "Failed to retrieve data"
+            "message": "both email & phoneNumber are null"
         })
     }
-    
-    res.status(200).json({
-        "status": "Success",
-        "message": "data"
-    })
 }
 
 export default { getWelcomeMessage, findIdentity };
